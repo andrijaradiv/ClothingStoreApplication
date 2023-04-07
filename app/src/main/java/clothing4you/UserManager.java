fc4b2df95389816188a642a4bffe36a72069a6eb
gabikir99 <99775013+gabikir99@users.noreply.github.com>
2023-04-06 20:23:13 -0400

    comment UserManger.java

--- app/src/main/java/clothing4you/UserManager.java
+++ app/src/main/java/clothing4you/UserManager.java
@@ -5,11 +5,11 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
+// This class provides methods for registering and logging in users
 public class UserManager {
 
     private UserManager(){}
-
+    //using the JDBC class to communicate with a database, we create users 
     public static void register(String name, String email, String username, String password, String table){
         try {
             Connection conn = JDBC.establishConnection();
@@ -21,11 +21,11 @@ public class UserManager {
             throw new RuntimeException(ex);
         }
     }
-
+    
     public static void register(String name, String email, String username, String password){
         register(name, email, username, password, "users");
     }
-
+    //checks if the username and password exist in the JDBC class
     public static boolean login(String username, String password, String table){
         try {
             if(JDBC.exists(username, table, "username")){
