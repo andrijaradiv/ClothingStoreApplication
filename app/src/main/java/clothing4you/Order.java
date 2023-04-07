66eaec33680564cb00eec7661ae4a0d07b18d22d
gabikir99 <99775013+gabikir99@users.noreply.github.com>
2023-04-06 20:10:05 -0400

    comment on order.java

--- app/src/main/java/clothing4you/Order.java
+++ app/src/main/java/clothing4you/Order.java
@@ -1,5 +1,5 @@
 package clothing4you;
-
+//this is the object that contains the info about x order (date it was bought, order number, and amount that was paid)
 public class Order {
     private String date;
     private String orderNumber;
@@ -10,7 +10,8 @@ public class Order {
         this.orderNumber = orderNumber;
         this.amount = amount;
     }
-
+    
+    //all the getters and setters for the order object 
     public String getDate() {
         return date;
     }
